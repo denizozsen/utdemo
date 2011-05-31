7a54b70e1098c964af414bff1ae97273bc87dd1c
Deniz <deniz.ozsen@gmail.com>
2011-05-31 10:51:31 +0400

    Add skeleton of function library utilities.php

---
+++ includes/utilities.php
@@ -0,0 +1,16 @@
+<?php
+
+function sendEmailToCustomer($customer, $subject, $body)
+{
+	// TODO - implement
+}
+
+function sendOrderConfirmationEmail($customer, $order, $subject, $body)
+{
+	// TODO - implement
+}
+
+function sendPromotionEmail($customer, $products, $subject, $body)
+{
+	// TODO - implement
+}
