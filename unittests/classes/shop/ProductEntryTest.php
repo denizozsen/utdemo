908c73ee3a2c80ad1854cb9ccb0ddb95d0b730b4
Deniz <deniz.ozsen@gmail.com>
2011-05-31 15:18:28 +0400

    Make first constructor unit test work for class ProductEntry

--- unittests/classes/shop/ProductEntryTest.php
+++ unittests/classes/shop/ProductEntryTest.php
@@ -2,6 +2,9 @@
 
 require_once 'classes/shop/ProductEntry.php';
 
+require_once 'unittests/classes/shop/stubs/StubProduct.php';
+require_once 'unittests/classes/shop/stubs/StubProductEntry.php';
+
 require_once 'PHPUnit/Framework/TestCase.php';
 
 /**
@@ -25,38 +28,23 @@ class ProductEntryTest extends PHPUnit_Framework_TestCase
 		parent::tearDown();
 	}
 	
-	/**
-	 * Tests ProductEntry->__construct()
-	 */
 	public function testConstructor_noExceptions()
 	{
-		// TODO Auto-generated ProductEntryTest->test__construct()
-		$this->markTestIncomplete ( "__construct test not implemented" );
-		
-		new ProductEntry(null, -1);
+		new ProductEntry(new StubProduct(), 1);
 	}
 	
-	/**
-	 * Tests ProductEntry->getProduct()
-	 */
 	public function testGetProduct()
 	{
 		// TODO Auto-generated ProductEntryTest->testGetProduct()
 		$this->markTestIncomplete ( "getProduct test not implemented" );
 	}
 	
-	/**
-	 * Tests ProductEntry->getQuantity()
-	 */
 	public function testGetQuantity()
 	{
 		// TODO Auto-generated ProductEntryTest->testGetQuantity()
 		$this->markTestIncomplete ( "getQuantity test not implemented" );
 	}
 	
-	/**
-	 * Tests ProductEntry->setQuantity()
-	 */
 	public function testSetQuantity()
 	{
 		// TODO Auto-generated ProductEntryTest->testSetQuantity()
